48a107bf0e5d3f5efabfcf8a8c605b6d5791d09d
lyra833 <alxg833@gmail.com>
2015-01-17 13:34:56 -0500

    directory name change

---
+++ src/public/index.js
@@ -0,0 +1,20 @@
+function file_upload()
+{
+	$.ajax({
+		url: '/bitflogger/file-upload',
+		type: 'POST',
+		contentType: 'application/json',
+		dataType: 'json'});
+};
+
+$('#submit').click(function(e)
+{
+	e.preventDefault();
+	console.log("Button clicked!")
+	file_upload();	
+});
+
+socket.on('file-read', function()
+{
+	$('#download').show();
+});
