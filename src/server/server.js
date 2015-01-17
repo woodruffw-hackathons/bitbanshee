596d7960e3e6fba8bee5184eea8e24c2adcff023
William Woodruff <william@tuffbizz.com>
2015-01-17 16:48:51 -0500

    old

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.bodyParser({uploadDir: './uploads'}))
-
 app.post('/file-upload', function(req, res, next) {
 	console.log(req.files);
 });
