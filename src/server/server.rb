47fbe445a4a46da3231d3b28ac927e06a2104411
William Woodruff <william@tuffbizz.com>
2015-01-17 21:42:33 -0500

    final touches

--- src/server/server.rb
+++ src/server/server.rb
@@ -5,10 +5,22 @@ require "sinatra"
 
 post "/makebdiff" do
 	File.open('oldbin', "w") do |f|
-		f.write(params['file1'][:tempfile].read)
+		f.write(params['oldbin'][:tempfile].read)
 	end
 	File.open('newbin', "w") do |f|
-		f.write(params['file2'][:tempfile].read)
+		f.write(params['newbin'][:tempfile].read)
 	end
-	return "Good."
+	system("../bdiff/bdiff oldbin newbin diffbin")
+	return File.open('diffbin').read
+end
+
+post "/dopatch" do
+	File.open('oldbin', "w") do |f|
+		f.write(params['oldbin'][:tempfile].read)
+	end
+	File.open('diffbin', "w") do |f|
+		f.write(params['diffbin'][:tempfile].read)
+	end
+	system("../bdiff/bpatch oldbin diffbin newbin")
+	return File.open('newbin').read
 end
